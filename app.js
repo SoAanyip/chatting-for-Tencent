f5ae8c8f878d873146738fab5a6a09b9b561b1e1
AanyipSo <cvdura@hotmail.com>
2015-04-13 04:13:15 +0800

    update time, views

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+//var http = require('http');
 var io = require('socket.io')(http);
 var path = require('path');
+//var session = require('express-session');
+
+/*set port*/
+app.set('port', process.env.PORT || 3000);
 
 /*define where static files are*/
 app.use(express.static(__dirname + '/app'));
+//app.use(express.static(path.join(__dirname, 'app')));
 
-/*app.use(function(req,res){
-	res.sendFile(path.join(__dirname, '/app', 'index.html'));
-	//res.sendfile('./app/index.html');
+/*session settings*/
+/*app.use(session({
+  secret: 'chatting',
+  cookie: { maxAge: 60000*60*24*7 }
+}))
+
+app.use(function(req,res){
+	console.log('11111111111111111');
+	console.log(req.session);
 })*/
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/app', 'index.html'));
 });
 
 /*server functions*/
+var num = 0;
 io.on('connection',function(socket){
 	console.log('an user here!');
 	socket.broadcast.emit('welcome');
-	socket.on('sendAllMessage',function(msg){
+	socket.on('sendAllMessage',function(text){
+		var date = new Date();
+		var hour = date.getHours();
+		var min = date.getMinutes();
+		var sec = date.getSeconds();
+		if(hour.length === 1) hour = '0'+hour;
+		if(min.length === 1) min = '0'+min;
+		if(sec.length === 1) sec = '0'+sec;
+		var msg = {
+			name:'123',
+			time:hour+':'+min+':'+sec,
+			text:text
+		}
 		io.emit('sendAllMessage',msg);
-		console.log('message:' + msg);
+		console.log('message:' + text);
 	});
 	socket.on('disconnect',function(){
 		console.log('an user left!');
 	});
 })
 
+/*io.set('authorization',function(handshakeData,accept){
+	console.log(handshakeData.headers);
+	accept(null,true);
+});	*/
+
 /*get start*/
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(app.get('port'), function(){
+  console.log('listening on '+ app.get('port'));
 });
